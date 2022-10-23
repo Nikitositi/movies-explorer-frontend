cbe28e341d7ec8a76db0fb2bf5187d02a5bfc34e
Nikita Shcherbakha <nikitositi99@yandex.ru>
2022-10-23 17:11:43 +1000

    коррекция адаптива

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,20 +1,33 @@
-import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 
 function MoviesCard(props) {
   const { image, title, duration, isSaved } = props;
   const { pathname } = useLocation();
-  useEffect;
+  const handleClick = (evt) => {
+    const btn = evt.target;
+    btn.textContent = '';
+    btn.classList.add('movies-card__btn_success');
+  };
+
+  const handleDelete = (evt) => {
+    const btn = evt.target;
+    btn.closest('.movies-card').remove();
+  };
 
   return (
     <li className='movies-card'>
       <div className='movies-card__img-wrapper'>
         <img className='movies-card__image' src={image} alt={title} />
         {pathname === '/saved-movies' ? (
-          <button className='movies-card__btn movies-card__btn_delete' />
+          <button
+            className='movies-card__btn movies-card__btn_delete'
+            onClick={handleDelete}
+          />
         ) : isSaved ? (
-          <button className='movies-card__btn'>Сохранить</button>
+          <button className='movies-card__btn' onClick={handleClick}>
+            Сохранить
+          </button>
         ) : (
           <button className='movies-card__btn movies-card__btn_success' />
         )}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,27 +8,37 @@ const movies = [
   },
   {
     url: movieCard,
-    title: '33 слова о дизайне',
+    title: 'Киноальманах «100 лет дизайна',
     duration: '1ч 17м',
   },
   {
     url: movieCard,
-    title: '33 слова о дизайне',
+    title: 'В погоне за Бенкси',
     duration: '1ч 17м',
   },
   {
     url: movieCard,
-    title: '33 слова о дизайне',
+    title: 'Баския: Взрыв реальности',
     duration: '1ч 17м',
   },
   {
     url: movieCard,
-    title: '33 слова о дизайне',
+    title: 'Бег это свобода',
     duration: '1ч 17м',
   },
   {
     url: movieCard,
-    title: '33 слова о дизайне',
+    title: 'Книготорговцы',
+    duration: '1ч 17м',
+  },
+  {
+    url: movieCard,
+    title: 'Когда я думаю о Германии ночью',
+    duration: '1ч 17м',
+  },
+  {
+    url: movieCard,
+    title: 'Gimme Danger: История Игги и The Stooges',
     duration: '1ч 17м',
   },
 ];
